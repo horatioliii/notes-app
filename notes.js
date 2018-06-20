a90c4a8720ee2bedcfb7aaead23433a86f545ec5
Shuanghao <li407@indiana.edu>
2018-06-19 17:11:01 -0700

    first commit

---
+++ notes.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+let fetchNotes = () => {
+    try {
+        let noteString = fs.readFileSync('notesData.json');
+        return JSON.parse(noteString);
+    } catch (e) {
+        return [];
+    }
+};
+
+let saveNotes = (notes) => {
+    fs.writeFileSync('notesData.json', JSON.stringify(notes));
+};
+
+let addNote = (title, body) => {
+    let notes = fetchNotes();
+    let note = {
+        title,
+        body
+    };
+    let dupNotes = notes.filter((note) =>  note.title === title);
+
+    if (dupNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+};
+
+let getAll = () => {
+    return fetchNotes();
+}
+
+let getNote = (title) => {
+    let notes = fetchNotes();
+    let note = notes.filter((note) => note.title === title);
+    return note[0];
+}
+
+let removeNote = (title) => {
+    let notes = fetchNotes();
+    let filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
+}
+
+let logNote = (note) => {
+    console.log('-----------');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote,
+    logNote
+};
